7b7e4bfec15b9794a20db60d1ab0ae42327431f9
AndyBursh <andy.p.hunt91@gmail.com>
2013-08-17 17:16:36 +0100

    Some basic unit tests.
    I really should add a more complete and thorough set.

---
+++ Evolve2.Tests/Mocks/MockGuidIdentityProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolve2.Tests.Mocks
+{
+    public class MockGuidIdentityProvider : Evolve2.Util.IIdentityProvider<Guid>
+    {
+        private string _guidString;
+        public MockGuidIdentityProvider(string guidString)
+        {
+            _guidString = guidString;
+        }
+
+        public Guid NewIdentity()
+        {
+            return new Guid(_guidString);
+        }
+
+        public bool Equals(Guid a, Guid b)
+        {
+            return a == b;
+        }
+    }
+}
---
+++ Evolve2.Tests/Vertex.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Evolve2;
+using Evolve2.Util;
+using Evolve2.Tests.Mocks;
+
+namespace Evolve2.Tests
+{
+    [TestClass]
+    public class Vertex
+    {
+        private MockGuidIdentityProvider guidProvider;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            guidProvider = new MockGuidIdentityProvider("8ADDEB1A-04B5-442C-A037-59777350F0B5");
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            guidProvider = null;
+        }
+
+        [TestMethod]
+        public void State_HealthyByDefault()
+        {
+            Vertex<Guid> v = new Vertex<Guid>(guidProvider);
+            Assert.AreEqual<State>(v.State, State.HEALTHY);
+        }
+
+        [TestMethod]
+        public void State_HealthyBySetting()
+        {
+            Vertex<Guid> v = new Vertex<Guid>(guidProvider, State.HEALTHY);
+            Assert.AreEqual<State>(v.State, State.HEALTHY);
+        }
+
+        [TestMethod]
+        public void State_MutantBySetting()
+        {
+            Vertex<Guid> v = new Vertex<Guid>(guidProvider, State.MUTANT);
+            Assert.AreEqual<State>(v.State, State.MUTANT);
+        }
+
+        [TestMethod]
+        public void SwitchState_MutantToHealthy()
+        {
+            Vertex<Guid> v = new Vertex<Guid>(guidProvider, State.MUTANT);
+
+            Assert.AreEqual<State>(v.State, State.MUTANT);
+            v.SwitchState(State.HEALTHY);
+            Assert.AreEqual<State>(v.State, State.HEALTHY);
+        }
+
+        [TestMethod]
+        public void SwitchState_HealthyToMutant()
+        {
+            Vertex<Guid> v = new Vertex<Guid>(guidProvider, State.HEALTHY);
+
+            Assert.AreEqual<State>(v.State, State.HEALTHY);
+            v.SwitchState(State.MUTANT);
+            Assert.AreEqual<State>(v.State, State.MUTANT);
+        }
+
+        [TestMethod]
+        public void Clone_ObjectReferencesNotEqual()
+        {
+            Vertex<Guid> v = new Vertex<Guid>(guidProvider);
+            Vertex<Guid> v2 = (Vertex<Guid>)v.Clone();
+
+            Assert.AreNotSame(v2, v);
+            Assert.AreNotEqual(v2, v);
+        }
+
+        [TestMethod]
+        public void Clone_StatesClonedCorrectly()
+        {
+            Vertex<Guid> v = new Vertex<Guid>(guidProvider);
+            Vertex<Guid> v2 = (Vertex<Guid>)v.Clone();
+
+            Assert.AreEqual<State>(v.State, v2.State);
+        }
+
+        [TestMethod]
+        public void Clone_IdentityClonedCorrectly()
+        {
+            Vertex<Guid> v = new Vertex<Guid>(guidProvider);
+            Vertex<Guid> v2 = (Vertex<Guid>)v.Clone();
+
+            Assert.AreEqual<Guid>(v.Identity, v2.Identity);
+            Assert.AreNotSame(v.Identity, v2.Identity);
+        }
+    }
+}
