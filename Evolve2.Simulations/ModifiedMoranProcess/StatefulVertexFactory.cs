3251a718a7da246c136f7c19a33df32e0f63070a
AndyBursh <andy.p.hunt91@gmail.com>
2013-08-23 22:36:41 +0100

    Implementation of IVertexFactory for StatefulVertex
    Created an implementation of IVertexFactory for StatefulVertex to allow
    graph type helpers to be used with the modified moran process

---
+++ Evolve2.Simulations/ModifiedMoranProcess/StatefulVertexFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Evolve2.Util.Factories;
+using Evolve2.State;
+
+namespace Evolve2.Simulations.ModifiedMoranProcess
+{
+    public class StatefulVertexFactory<TState> : StatefulVertexFactory<Guid, TState>
+        where TState : struct
+    {
+        public StatefulVertexFactory(IState<TState> DefaultState)
+            : base(DefaultState)
+        { }
+    }
+
+    public class StatefulVertexFactory<T, TState> : IVertexFactory<T>
+        where T : struct
+        where TState : struct
+    {
+        private IState<TState> _defaultState;
+        public StatefulVertexFactory(IState<TState> DefaultState)
+        {
+            _defaultState = DefaultState;
+        }
+
+        public Vertex<T> NewVertex(Util.IIdentityProvider<T> IdentityProvider)
+        {
+            return new StatefulVertex<T, TState>((IState<TState>)_defaultState.Clone(), IdentityProvider);
+        }
+    }
+}
