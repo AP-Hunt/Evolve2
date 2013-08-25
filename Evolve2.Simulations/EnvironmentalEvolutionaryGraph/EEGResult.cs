349febbabe31d965133d71ed9aac97452487a639
AndyBursh <andy.p.hunt91@gmail.com>
2013-08-25 14:34:30 +0100

    Added simulation result class for EEG

---
+++ Evolve2.Simulations/EnvironmentalEvolutionaryGraph/EEGResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolve2.Simulations.EnvironmentalEvolutionaryGraph
+{
+    public class EEGResult
+    {
+        public int RepetitionsPerformed { get; set; }
+        public int Fixations { get; set; }
+        public int Extinctions { get; set; }
+        public int Timeout { get; set; }
+
+        public double FixationProbability
+        {
+            get
+            {
+                return ((double)Fixations / (double)RepetitionsPerformed);
+            }
+        }
+
+        public double ExtinctionProbability
+        {
+            get
+            {
+                return ((double)Extinctions / (double)RepetitionsPerformed);
+            }
+        }
+
+        public double TimeoutProbability
+        {
+            get
+            {
+                return ((double)Timeout / (double)RepetitionsPerformed);
+            }
+        }
+    }
+}
