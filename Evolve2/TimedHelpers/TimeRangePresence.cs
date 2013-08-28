e21c88cbc24c67ac34f8e7fb345cc7cd36d18970
AndyBursh <andy.p.hunt91@gmail.com>
2013-08-28 20:57:49 +0100

    Added helper for valid time ranges
    Also added exception for time ranges going out of range

---
+++ Evolve2/TimedHelpers/TimeRangePresence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolve2.TimedHelpers
+{
+    public static class TimeRangePresence
+    {
+        public static Func<Graph<TIdentity>, int, IEnumerable<TIdentity>, bool> PresentBetween<TIdentity>(int Min, int Max)
+            where TIdentity : struct
+        {
+            return (g, t, v) => (t >= Min && t <= Max)? true : false;
+        }
+    }
+}
