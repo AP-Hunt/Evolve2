a829e5d4a86bc16a20fe7e74e02af08ff8b2cc8d
AndyBursh <andy.p.hunt91@gmail.com>
2013-08-28 20:28:33 +0100

    Added timed graph elements
    New abstract time graph element and timed vertex implementation

---
+++ Evolve2/TimedVertex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Evolve2.Util;
+
+namespace Evolve2
+{
+    public class TimedVertex<TIdentity> : TimedGraphElement<TIdentity>
+        where TIdentity : struct
+    {
+        public TimedVertex(IIdentityProvider<TIdentity> IdentityProvider,
+                           Func<Graph<TIdentity>, int, IEnumerable<TIdentity>, bool> Present)
+            : base(IdentityProvider, Present)
+        { }
+    }
+}
