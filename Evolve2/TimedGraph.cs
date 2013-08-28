9adb64df214b9c2561b5d4a598d704e2751f1c6a
AndyBursh <andy.p.hunt91@gmail.com>
2013-08-28 21:03:47 +0100

    Added a small amount of documentation to the graph

--- Evolve2/TimedGraph.cs
+++ Evolve2/TimedGraph.cs
@@ -16,6 +16,9 @@ namespace Evolve2
         private Dictionary<int, ICollection<Vertex<TIdentity>>> _cachedTimeStepVertices;
         private IEnumerable<Vertex<TIdentity>> _computedVertexSet;
 
+        /// <summary>
+        /// Set of vertices including all basic vertices, plus those timed vertices which appear at the current time step
+        /// </summary>
         public override IEnumerable<Vertex<TIdentity>> Vertices
         {
             get
@@ -33,6 +36,9 @@ namespace Evolve2
             _timedVertices = new List<TimedVertex<TIdentity>>();
         }
 
+        /// <summary>
+        /// Move to the next time step
+        /// </summary>
         public void Tick()
         {
             if (_currentTimeStep >= _maxTimeStep)
@@ -43,6 +49,10 @@ namespace Evolve2
             Tick(_currentTimeStep++);
         }
 
+        /// <summary>
+        /// Move to the specified time step
+        /// </summary>
+        /// <param name="NextStep"></param>
         public void Tick(int NextStep)
         {
             if (NextStep > _maxTimeStep)
@@ -50,13 +60,22 @@ namespace Evolve2
                 throw new ArgumentOutOfRangeException("NextStep", "NextStep cannot be greater than the maximum step value of the graph");
             }
             _currentTimeStep = NextStep;
+            recomputeVertexSet(_currentTimeStep);
         }
 
+        /// <summary>
+        /// Set the largest possible time step value
+        /// </summary>
+        /// <param name="MaxSteps"></param>
         public void SetMaxTimeSteps(int MaxSteps)
         {
             _maxTimeStep = MaxSteps;
         }
 
+        /// <summary>
+        /// Add a timed vertex to the graph.
+        /// </summary>
+        /// <param name="Vertex"></param>
         public void AddTimedVertex(TimedVertex<TIdentity> Vertex)
         {
             if (Vertex == null)
@@ -73,9 +92,14 @@ namespace Evolve2
                 }
             }
 
-            computeVertexSet(_currentTimeStep);
+            recomputeVertexSet(_currentTimeStep);
         }
 
+        /// <summary>
+        /// Adds a timed vertex to the cache of the timed vertices
+        /// </summary>
+        /// <param name="T"></param>
+        /// <param name="Vertex"></param>
         private void addToTimedVerticesCache(int T, TimedVertex<TIdentity> Vertex)
         {
             if (!_cachedTimeStepVertices.ContainsKey(T))
@@ -86,7 +110,11 @@ namespace Evolve2
             _cachedTimeStepVertices[T].Add(Vertex);
         }
 
-        private void computeVertexSet(int T)
+        /// <summary>
+        /// Recomputes the vertex set for the given timestep
+        /// </summary>
+        /// <param name="T"></param>
+        private void recomputeVertexSet(int T)
         {
             IEnumerable<Vertex<TIdentity>> vertices = base.Vertices;
             vertices.Union(cachedVerticesAtTimeStep(T));
@@ -94,6 +122,11 @@ namespace Evolve2
             _computedVertexSet = vertices;
         }
 
+        /// <summary>
+        /// Gets the timed vertices which appear the given timestep
+        /// </summary>
+        /// <param name="T"></param>
+        /// <returns></returns>
         private IEnumerable<Vertex<TIdentity>> cachedVerticesAtTimeStep(int T)
         {
             if (_cachedTimeStepVertices.ContainsKey(T))
